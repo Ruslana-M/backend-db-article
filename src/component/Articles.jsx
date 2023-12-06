e753c78926b6161e8cedf4ea81e228a41da01504
Ruslana Mysholovka <r.myshelovka@me.com>
2023-12-06 09:48:41 +0100

    add Object retrieval, useState and useEffect

--- src/component/Articles.jsx
+++ src/component/Articles.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import Backendless from 'backendless';
+import React, { useEffect, useState } from 'react'
 
 function Article() {
+const [articles, setArticles] =useState ([])
+  useEffect(() => {
+    Backendless.Data.of( "articles" ).find()
+ .then( res=>{console.log(res)
+  setArticles(res)
+  })
+ .catch( err =>{console.log(err)
+  });
+  }, [])
+
+  
+
+
   return (
     <div>Article</div>
   )
